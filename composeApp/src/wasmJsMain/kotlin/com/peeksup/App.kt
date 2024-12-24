fd2734bd5993a1f801fe33de4c76fba7f900536f
jAsOn <yhkim@dnable.xyz>
2024-12-24 15:52:36 +0900

    update

--- composeApp/src/wasmJsMain/kotlin/com/peeksup/App.kt
+++ composeApp/src/wasmJsMain/kotlin/com/peeksup/App.kt
@@ -1,35 +1,23 @@
 package com.peeksup
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-
+import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import com.peeksup.food.FoodSurveyApp
+import org.jetbrains.compose.resources.Font
+import revoke.composeapp.generated.resources.Pretendard_SemiBold
 import revoke.composeapp.generated.resources.Res
-import revoke.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+    val font = Font(Res.font.Pretendard_SemiBold)
+
+    MaterialTheme(
+        typography = Typography(
+            FontFamily(font)
+        )
+    ) {
+        FoodSurveyApp()
     }
 }
