91a7b84e509b511a0d60ab3c9c5d7bff1a55d840
jAsOn <yhkim@dnable.xyz>
2024-12-25 13:13:03 +0900

    모바일에서 복사하기 변경

--- composeApp/src/wasmJsMain/kotlin/com/peeksup/food/CopyLinkButton.kt
+++ composeApp/src/wasmJsMain/kotlin/com/peeksup/food/CopyLinkButton.kt
@@ -19,7 +19,9 @@ import com.peeksup.util.LanguageManager
 import com.peeksup.util.StringKey
 import com.peeksup.util.URLUtils
 import com.peeksup.util.umami
+import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.HTMLTextAreaElement
 import kotlin.js.Promise
 
 @Composable
@@ -30,9 +32,29 @@ fun CopyLinkButton(responses: Map<String, String>) {
         onClick = {
             umami.track("copy")
             val shareUrl = URLUtils.encodeResponsesToURL(responses)
-            window.navigator.clipboard.writeText(shareUrl).then { _ ->
-                showCopySuccess = true
-                Promise.resolve(null)
+
+            try {
+                // Modern API 시도
+                window.navigator.clipboard.writeText(shareUrl).then { _ ->
+                    showCopySuccess = true
+                    Promise.resolve(null)
+                }
+            } catch (e: Exception) {
+                println("클립보드 접근 실패 $e")
+                val shareUrl2 = URLUtils.encodeResponsesToURL(responses)
+
+                val textArea = document.createElement("textarea") as HTMLTextAreaElement
+                textArea.value = shareUrl2
+                document.body?.appendChild(textArea)
+                textArea.select()
+
+                try {
+                    showCopySuccess = document.execCommand("copy")
+                } catch (e: Exception) {
+                    println(e)
+                } finally {
+                    document.body?.removeChild(textArea)
+                }
             }
         },
         colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF4CAF50)),
